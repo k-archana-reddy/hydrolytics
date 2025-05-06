e3eb58255049ef1bfbda0452da85a4181ee026c6
Archana <karchanareddy16@gmail.com>
2025-05-06 09:24:14 +0530

    `Updated login.html, settings.py, urls.py and added GSAP animation for hero section`

--- waternet/urls.py
+++ waternet/urls.py
@@ -13,7 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# Old import
+# from django.conf.urls import url
+
+# New import
+from django.urls import re_path
 from django.contrib import admin
 from Remote_User import views as remoteuser
 from waternet import settings
@@ -22,19 +26,19 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^$', remoteuser.login, name="login"),
-    url(r'^Register1/$', remoteuser.Register1, name="Register1"),
-    url(r'^Prediction_Water_Quality_Detection/$', remoteuser.Prediction_Water_Quality_Detection, name="Prediction_Water_Quality_Detection"),
-    url(r'^ViewYourProfile/$', remoteuser.ViewYourProfile, name="ViewYourProfile"),
-    url(r'^serviceproviderlogin/$',serviceprovider.serviceproviderlogin, name="serviceproviderlogin"),
-    url(r'View_Remote_Users/$',serviceprovider.View_Remote_Users,name="View_Remote_Users"),
-    url(r'^charts/(?P<chart_type>\w+)', serviceprovider.charts,name="charts"),
-    url(r'^charts1/(?P<chart_type>\w+)', serviceprovider.charts1, name="charts1"),
-    url(r'^likeschart/(?P<like_chart>\w+)', serviceprovider.likeschart, name="likeschart"),
-    url(r'^Find_Predicted_Water_Quality_Detection_Type_Ratio/$', serviceprovider.Find_Predicted_Water_Quality_Detection_Type_Ratio, name="Find_Predicted_Water_Quality_Detection_Type_Ratio"),
-    url(r'^Train_Test_DataSets/$', serviceprovider.Train_Test_DataSets, name="Train_Test_DataSets"),
-    url(r'^View_Predicted_Water_Quality_Detection_Type/$', serviceprovider.View_Predicted_Water_Quality_Detection_Type, name="View_Predicted_Water_Quality_Detection_Type"),
-    url(r'^Download_Predicted_DataSets/$', serviceprovider.Download_Predicted_DataSets, name="Download_Predicted_DataSets"),
+    re_path('admin/', admin.site.urls),
+    re_path(r'^$', remoteuser.login, name="login"),
+    re_path(r'^Register1/$', remoteuser.Register1, name="Register1"),
+    re_path(r'^Prediction_Water_Quality_Detection/$', remoteuser.Prediction_Water_Quality_Detection, name="Prediction_Water_Quality_Detection"),
+    re_path(r'^ViewYourProfile/$', remoteuser.ViewYourProfile, name="ViewYourProfile"),
+    re_path(r'^serviceproviderlogin/$',serviceprovider.serviceproviderlogin, name="serviceproviderlogin"),
+    re_path(r'View_Remote_Users/$',serviceprovider.View_Remote_Users,name="View_Remote_Users"),
+    re_path(r'^charts/(?P<chart_type>\w+)', serviceprovider.charts,name="charts"),
+    re_path(r'^charts1/(?P<chart_type>\w+)', serviceprovider.charts1, name="charts1"),
+    re_path(r'^likeschart/(?P<like_chart>\w+)', serviceprovider.likeschart, name="likeschart"),
+    re_path(r'^Find_Predicted_Water_Quality_Detection_Type_Ratio/$', serviceprovider.Find_Predicted_Water_Quality_Detection_Type_Ratio, name="Find_Predicted_Water_Quality_Detection_Type_Ratio"),
+    re_path(r'^Train_Test_DataSets/$', serviceprovider.Train_Test_DataSets, name="Train_Test_DataSets"),
+    re_path(r'^View_Predicted_Water_Quality_Detection_Type/$', serviceprovider.View_Predicted_Water_Quality_Detection_Type, name="View_Predicted_Water_Quality_Detection_Type"),
+    re_path(r'^Download_Predicted_DataSets/$', serviceprovider.Download_Predicted_DataSets, name="Download_Predicted_DataSets"),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
